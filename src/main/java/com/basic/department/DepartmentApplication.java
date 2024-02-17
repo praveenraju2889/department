fc4803077c019630b72a226203ae02d439cbee82
Praveen Raju <praveenraju2889@gmail.com>
2024-02-18 00:23:36 +0400

    JasyptEncryptorConfig added

--- src/main/java/com/basic/department/DepartmentApplication.java
+++ src/main/java/com/basic/department/DepartmentApplication.java
@@ -2,8 +2,10 @@ package com.basic.department;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class DepartmentApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/basic/department/DepartmentApplicationTests.java
+++ src/test/java/com/basic/department/DepartmentApplicationTests.java
@@ -1,13 +1,30 @@
 package com.basic.department;
 
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class DepartmentApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@SuppressWarnings("unused")
+	@Test
+	public void getTestEncryptor() {
+		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword("deptencryptkey"); // encryptor's private key
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
 	}
 
 }
